bc4d94b0b5de6a3d01c18e921d1d9456a3b6af6d
Igor Demovic <igor.demovic@gmail.com>
2023-09-24 13:28:26 +0200

    Filtering some requests

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@ using System;
 using Serilog;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
-using Serilog.Events;
-using Serilog.Sinks.Logz.Io;
 using ServiceStack;
 
 try
@@ -11,21 +9,15 @@ try
 	var builder = WebApplication.CreateBuilder(args);
 
 	builder.Host.UseSerilog((ctx, lc) => lc
+    .Filter.ByExcluding(c => c.Properties["RequestPath"].ToString().StartsWith("/swagger-ui"))
 		.Enrich.FromLogContext()
 		.WriteTo.Console()
 		.WriteTo.Debug()
 		.WriteTo.Seq(serverUrl: builder.Configuration["ConnectionStrings:Seq"], apiKey: builder.Configuration["AppSettings:SeqToken"])
-		// .WriteTo.LogzIo(builder.Configuration["AppSettings:LogzioToken"], "ATS.DarkSearch",
-		// 	new LogzioOptions 
-		// 	{ 
-		// 		RestrictedToMinimumLevel = LogEventLevel.Debug,
-		// 		Period = TimeSpan.FromSeconds(15),
-		// 		LogEventsInBatchLimit = 50
-		// 	})
 		.WriteTo.File(path: "~Logs/log.txt".MapServerPath(), rollingInterval: RollingInterval.Day)
 		.ReadFrom.Configuration(ctx.Configuration)
 	);
-	
+
 	var app = builder.Build();
 	if (app.Environment.IsDevelopment())
 	{
@@ -40,9 +32,9 @@ try
 	}
 
 	app.UseSerilogRequestLogging();
-	
+
 	Licensing.RegisterLicense("18238-e1JlZjoxODIzOCxOYW1lOkVMSUEgcy5yLm8uLFR5cGU6SW5kaWUsTWV0YTowLEhhc2g6UVI3WUI4UFpYOTlDY1Q0WWQ5bVpiYnQrRUorRG5kUldVVVYrdE1tMlVsWWl3S3pQb010ME4yVW1PR0g0VkFtUktDMW5zOWtYUlFKOFAyVHh3Sm8va0VHWVpVanZsZWZuOFlmeUkwNWFQbDI1QnRrWVBkNjVFcUk5OWhWb0RKSkM0R0dPOS9RZkpyR3JQVTRoT1l3VEhYV1BqdnFlaXl3RlVnd1lqTEQyVmdRPSxFeHBpcnk6MjAyMi0xMC0xM30=");
-	
+
 	Log.Information("Starting web host");
 	app.Run();
 }
