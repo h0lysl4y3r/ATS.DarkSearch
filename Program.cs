56087ec92957fc2b63905772dbb64a7022a7151b
Igor Demovic <igor.demovic@gmail.com>
2024-02-21 21:00:26 +0100

    Removed excessive logging

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ try
 {
 	var builder = WebApplication.CreateBuilder(args);
 
-	builder.Host.UseSerilog((ctx, lc) => lc
-		.Enrich.FromLogContext()
-		.WriteTo.Console()
-		.WriteTo.Debug()
-		.WriteTo.Seq(serverUrl: builder.Configuration["ConnectionStrings:Seq"], apiKey: builder.Configuration["AppSettings:SeqToken"])
-		.WriteTo.File(path: "~Logs/log.txt".MapServerPath(), rollingInterval: RollingInterval.Day)
-		.ReadFrom.Configuration(ctx.Configuration)
+	builder.Host
+		.UseSerilog((ctx, lc) => lc
+			.Enrich.FromLogContext()
+			.WriteTo.Console()
+			.WriteTo.Debug()
+			.WriteTo.Seq(serverUrl: builder.Configuration["ConnectionStrings:Seq"], apiKey: builder.Configuration["AppSettings:SeqToken"])
+			.WriteTo.File(path: "~Logs/log.txt".MapServerPath(), rollingInterval: RollingInterval.Day)
+			.ReadFrom.Configuration(ctx.Configuration)
 	);
 
 	var app = builder.Build();
